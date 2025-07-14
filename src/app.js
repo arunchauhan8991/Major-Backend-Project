babde21cfcecc524f0e0b56d5c5c8330d2b9016c
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-07-14 23:02:33 +0530

    all setup completed

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 // app.use to use middleware
 
 
+// CONFIGURATION
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN, // Frontend URL
@@ -14,19 +16,27 @@ app.use(
   })
 );
 
+app.use(express.json({ limit: "16kb" })); //body-parser now included in express by default to parse json data.
 
-app.use(express.json({limit: "16kb"})) //body-parser now included in express by default to parse json data.
-
-
-app.use(express.urlencoded({ extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // extended: false: Uses the querystring library (can only parse simple key-value pairs)  DEFAULT VALUE
 // extended: true: Uses the qs library (can parse nested objects and arrays)
 
-
-app.use(express.static("public"))
+app.use(express.static("public"));
 //  to serve static files (like HTML, CSS, JS, images) from the "public" folder
 
+app.use(cookieParser());
 
-app.use(cookieParser())
+// ROUTES
 
+//import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+
+//  http://localhost:8000/api/v1/users/register
 export { app };
